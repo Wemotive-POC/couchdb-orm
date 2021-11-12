07e6bd23b8f2868c50ab0b403410295c5b749f63
Irene <irene@wemotiveforge.com>
2021-11-12 15:04:44 +0530

    Fix eslint and update code accordingly

--- example/schema.js
+++ example/schema.js
@@ -1,105 +1,105 @@
 import { Schema } from "../lib/index.js";
 
 const schema = Schema({
-	name: {
-		required: true,
-		type: String
-	},
+    name: {
+        required: true,
+        type: String
+    },
 
-	phoneNo: {
-		required: true,
-		type: String,
-		// The validate function must not depend on any state of the file
-		// It will be passed as is to backend
-		validate: (phoneNo) => {
-			if (!phoneNo.match(/\d{10}/))
-				throw({ forbidden: "Invalid Phone Number" })
-		}
-	},
+    phoneNo: {
+        required: true,
+        type: String,
+        // The validate function must not depend on any state of the file
+        // It will be passed as is to backend
+        validate: (phoneNo) => {
+            if (!phoneNo.match(/\d{10}/))
+                throw({ forbidden: "Invalid Phone Number" });
+        }
+    },
 
-	address: {
-		required: false,
-		type: {
-			address: {
-				required: true,
-				type: String
-			},
-			lane: {
-				required: false,
-				type: {
-					lane1: {
-						required: true,
-						type: String
-					},
-					lane2: {
-						required: false,
-						type: String
-					}
-				}
-			},
-			pincode: {
-				required: true,
-				type: Number
-			}
-		}
-	}
+    address: {
+        required: false,
+        type: {
+            address: {
+                required: true,
+                type: String
+            },
+            lane: {
+                required: false,
+                type: {
+                    lane1: {
+                        required: true,
+                        type: String
+                    },
+                    lane2: {
+                        required: false,
+                        type: String
+                    }
+                }
+            },
+            pincode: {
+                required: true,
+                type: Number
+            }
+        }
+    }
 });
 
 const docs = [
-	{
-		name: "Irene"
-	},
+    {
+        name: "Irene"
+    },
 
-	{
-		phoneNo: "9999999999"
-	},
+    {
+        phoneNo: "9999999999"
+    },
 
-	{
-		name: "Irene",
-		phoneNo: "9999999999"
-	},
+    {
+        name: "Irene",
+        phoneNo: "9999999999"
+    },
 
-	{
-		name: "Irene",
-		phoneNo: "abcdefghijk"
-	},
+    {
+        name: "Irene",
+        phoneNo: "abcdefghijk"
+    },
 
-	{
-		name: "Irene",
-		phoneNo: "9999999999",
-		address: {}
-	},
-	{
-		name: "Irene",
-		phoneNo: "9999999999",
-		address: {
-			address: "foo-bar",
-			pincode: "190014"
-		}
-	},
-	{
-		name: "Irene",
-		phoneNo: "9999999999",
-		address: {
-			address: "foo-bar",
-			lane: {
-				lane1: "Blah"
-			},
-			pincode: 190014
-		}
-	},
-	{
-		name: "Irene",
-		phoneNo: "9999999999",
-		address: {
-			address: "foo-bar",
-			lane: {
-				lane1: "Blah",
-				lane2: 15710
-			},
-			pincode: 190014
-		}
-	},
+    {
+        name: "Irene",
+        phoneNo: "9999999999",
+        address: {}
+    },
+    {
+        name: "Irene",
+        phoneNo: "9999999999",
+        address: {
+            address: "foo-bar",
+            pincode: "190014"
+        }
+    },
+    {
+        name: "Irene",
+        phoneNo: "9999999999",
+        address: {
+            address: "foo-bar",
+            lane: {
+                lane1: "Blah"
+            },
+            pincode: 190014
+        }
+    },
+    {
+        name: "Irene",
+        phoneNo: "9999999999",
+        address: {
+            address: "foo-bar",
+            lane: {
+                lane1: "Blah",
+                lane2: 15710
+            },
+            pincode: 190014
+        }
+    },
 ];
 console.log(schema.schema);
-docs.forEach(i => { console.log(i); console.log(schema.isValidDoc(i, true), "\n\n"); })
+docs.forEach(i => { console.log(i); console.log(schema.isValidDoc(i, true), "\n\n"); });
