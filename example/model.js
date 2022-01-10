0760dc3d95b7830179a1a47b8cf586cc7037c8d5
Irene <irene@wemotiveforge.com>
2022-01-10 14:41:09 +0530

    Add support for creating and querying design documents

--- example/model.js
+++ example/model.js
@@ -1,5 +1,5 @@
 const { env } = require("process");
-const { Connection, Model, Schema  } = require("../lib/index.js");
+const { Connection, Model, Schema, emit } = require("../lib/index.js");
 
 
 const userSchema = Schema({
@@ -26,7 +26,29 @@ const userSchema = Schema({
         },
         ["name"],
         [],
-    ]
+    ],
+    ddocs: {
+        email: {
+            views: {
+                count: {
+                    map: function (doc) {
+                        emit(doc.email, 1);
+                    },
+                    reduce: "_count"
+                }
+            }
+        }
+    }
+});
+
+userSchema.addDDoc("name", {
+    views: {
+        letters: {
+            map: function (doc) {
+                emit(doc._id, doc.name.length);
+            }
+        }
+    }
 });
 
 const conn = Connection(env.COUCH_DB_URL);
@@ -71,14 +93,25 @@ const User = Model(userSchema, { defaultDB: "users", dbPrefix: "users_" })(conn)
     const fooFromDb = await dummyOrg.get(foo.id);
     console.log(fooFromDb);
 
+    console.log("Getting one by email: foo@bar.com");
     const one = await dummyOrg.findOne({selector: {  email: "foo@bar.com"  }});
     console.log(one);
 
+    console.log("Getting all by name: Irene");
     const all = await dummyOrg.findAll({ selector: { name: "Irene" } });
     console.log(all.docs);
 
+    console.log("Getting all paginated with limit=2 skip=0");
     const paginated = await dummyOrg.findPaginated({ selector: {} }, 2, 0);
     console.log(paginated);
+
+    console.log("Getting data from design document email, view count");
+    const emailCount = await dummyOrg.ddoc.email.count();
+    console.log(emailCount);
+
+    console.log("Getting data from design document name, view letters");
+    const letters = await dummyOrg.ddoc.name.letters();
+    console.log(letters);
 
     console.log("Replicating");
     await dummyOrg.replicate(spamOrg, { continuous: false });
